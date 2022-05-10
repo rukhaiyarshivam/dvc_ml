5b7065ef551468150bead9572b0c6c4eeeab9604
rukhaiyarshivam <rukhaiyarshivam@gmail.com>
2022-05-11 03:25:25 +0530

    update data import code

---
+++ src/stage_01_load_save.py
@@ -0,0 +1,28 @@
+import os
+from src.utils.all_utils import read_yaml,createDirectory
+import argparse
+import pandas as pd
+
+def get_data(config_path):
+  print(config_path)
+  config=read_yaml(config_path)
+  remote_data_path=config['data_source']
+  df=pd.read_csv(remote_data_path,sep=";")
+  artifacts_directory=config['artifacts']['artifacts_dir']
+  artifacts_local_dir=config['artifacts']['raw_local_dir']
+  artifacts_local_file=config['artifacts']['raw_local_file']
+  # save dataset in local directory
+  raw_local_dir_path=os.path.join(artifacts_directory,artifacts_local_dir)
+  raw_local_dir_file_path=os.path.join(raw_local_dir_path,artifacts_local_file)
+  createDirectory([artifacts_directory,raw_local_dir_path])
+  df.to_csv(raw_local_dir_file_path,sep=",",index=False)
+  
+  
+
+
+if __name__=='__main__':
+  args=argparse.ArgumentParser()
+  args.add_argument("--config","-c",default="config/config.yaml")
+  parsed_args=args.parse_args()
+  get_data(parsed_args.config)
+  
--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,3 +1,4 @@
+from typing import List
 import yaml
 import os
 
@@ -6,5 +7,9 @@ import os
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
-
     return content
+
+def createDirectory(dirs:List):
+    for dir_path in dirs:
+        os.makedirs(dir_path,exist_ok=True)
+
